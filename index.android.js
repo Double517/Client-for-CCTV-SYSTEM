3578aaa0e96b5a68d993e6562945a82f1370f5c6
Ultrasteve <609117264@qq.com>
2017-03-06 14:54:31 +0800

    增加看直播的页面

--- index.android.js
+++ index.android.js
@@ -1,5 +1,10 @@
 import {AppRegistry} from 'react-native';
 import app from './app/main.js'
 
+import CCTVView from './app/View/CCTV_View.js'
 
-AppRegistry.registerComponent('CCTV', () => app);
+
+AppRegistry.registerComponent('CCTV', () => CCTVView);
+
+//AppRegistry.registerComponent('CCTV', () => app);
+
